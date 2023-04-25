61491d33ad7197440cc630c91cc346b3323d8e83
semaed <figsaned@gmail.com>
2023-04-25 17:30:16 +0000

    Write C program to execute in the custom shell

---
+++ execute.c
@@ -0,0 +1,45 @@
+#include "shell.h"
+
+/**
+ * execute - execute a command
+ * @args: array of arguments
+ * @envp: environment variables
+ *
+ * Return: 0 on success, -1 on failure
+ */
+int execute(char **args, char **envp)
+{
+	pid_t pid;/*process ID*/
+	int status;/*process status*/
+
+	if (args[0] == NULL)
+		return (1);
+
+	/*if the first argument is "exit" return -1 end exit the shell*/
+	if (strcmp(args[0], "exit") == 0)
+		return	(-1);
+
+	pid = fork();/*create a new process*/
+	if (pid == -1)/*check for errors in creating new process*/
+	{
+		perror("Fork error");
+		return (-1);
+	}
+	if (pid == 0)/*if in the child process*/
+	{
+		if (execve(args[0], args, envp) == -1)/*execute the command*/
+		{
+			/*print error message if command not found*/
+			printf("%s:command not foun\n", args[0]);
+			exit(EXIT_FAILURE);/*exit child process if command execution fails*/
+		}
+	}
+	else/*if in the parent process*/
+	{
+		do {
+			waitpid(pid, &status, WUNTRACED);/*wait for chil process to finish*/
+			/*check for process status*/
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	return (0);/*return success*/
+}
